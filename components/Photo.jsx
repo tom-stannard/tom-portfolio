9a07c4397e267bc17a9030814830df6c05345b6f
tom-stannard <tom_work@live.com>
2024-09-20 19:42:35 +1000

    fourth commit

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -12,7 +12,7 @@ const Photo = () => {
                 transition: {delay: 2, duration: 0.4, ease: "easeIn"},
                 }}
         >
-            {/* image */}
+            {/* tom image */}
             <motion.div 
             initial={{opacity: 0}} 
             animate={{
@@ -21,7 +21,7 @@ const Photo = () => {
             }}
             className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute">
                 <Image 
-                    src="assets/photo.png" 
+                    src="/assets/photo.png" 
                     priority 
                     quality={100} 
                     fill 
