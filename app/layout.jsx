ee0bc8c1657a87044312345f8655ede72a7ec69b
tom-stannard <tom_work@live.com>
2024-10-07 23:46:14 +1100

    eighteenth commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,7 +8,7 @@ import StairTransition from "@/components/StairTransition";
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
-  variable: '--font-jetbrainsMono',
+  variable: '--font-jetbrainsmono',
 });
 
 export const metadata = {
